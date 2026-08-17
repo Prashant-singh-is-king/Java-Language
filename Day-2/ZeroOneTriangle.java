f3ffb11f957c91c8df0e84ddeb502588da484ddc
Prashant Singh <Prashantsingh27112005@gmail.com>
2026-08-18 01:00:15 +0530

    Update a code

--- Day-2/ZeroOneTriangle.java
+++ Day-2/ZeroOneTriangle.java
@@ -6,16 +6,17 @@ public class ZeroOneTriangle {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=n ; j++){
+            for(int j=1 ; j<=i ; j++){
                 int sum = i+j;
                 if(sum % 2 == 0){
-                    System.out.print("1");
+                    System.out.print("1 ");
                 }
                 else{
-                    System.out.print("0");
+                    System.out.print("0 ");
                 }
-                System.out.println();
+                 
             }
+            System.out.println();
         }
         
     }
