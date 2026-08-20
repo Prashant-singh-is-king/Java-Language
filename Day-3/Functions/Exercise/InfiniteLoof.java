40d7ffc57844ba3a0192b581d2db39e35d8d0c0e
Prashant Singh <Prashantsingh27112005@gmail.com>
2026-08-20 22:53:15 +0530

    Add new Code

---
+++ Day-3/Functions/Exercise/InfiniteLoof.java
@@ -0,0 +1,11 @@
+//Write an infinite loop using do while condition.
+
+package Functions.Exercise;
+
+public class InfiniteLoof {
+    public static void main(String[] args) {
+        while (true) { 
+            System.out.println("Hello World");
+        }}
+    }
+
