18b53ddb22b2a128dc8632c9d19c7c8e63ffd685
Prashant Singh <Prashantsingh27112005@gmail.com>
2026-08-21 23:58:34 +0530

    add a code

---
+++ Day-3/Functions/Exercise/CountNumbers.java
@@ -0,0 +1,40 @@
+package Functions.Exercise;
+
+import java.util.Scanner;
+
+public class CountNumbers {
+
+    public static void countNumbers() {
+        Scanner sc = new Scanner(System.in);
+
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+        char choice;
+
+        do {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            if (num > 0) {
+                positive++;
+            } else if (num < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+
+            System.out.print("Do you want to enter another number? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Positive numbers: " + positive);
+        System.out.println("Negative numbers: " + negative);
+        System.out.println("Zeros: " + zero);
+    }
+
+    public static void main(String[] args) {
+        countNumbers();
+    }
+}
